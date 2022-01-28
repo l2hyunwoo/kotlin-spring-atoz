756a1d1c125b47e8c443872b762f834b9b26960b
Hyun Woo Lee <l2hyunwoo@gmail.com>
2022-01-29 02:46:30 +0900

    INIT COMMIT

---
+++ src/main/kotlin/com/l2hyunwoo/kotlinspringbootdemo/KotlinspringbootdemoApplication.kt
@@ -0,0 +1,11 @@
+package com.l2hyunwoo.kotlinspringbootdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinspringbootdemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinspringbootdemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/l2hyunwoo/kotlinspringbootdemo/KotlinspringbootdemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.l2hyunwoo.kotlinspringbootdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinspringbootdemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
