387f58f18ff1d35719c4769c8278801571117802
Hyun Woo Lee <l2hyunwoo@gmail.com>
2022-01-30 00:03:09 +0900

    Spring Security 설정

--- src/main/kotlin/com/l2hyunwoo/kotlinspringbootdemo/domain/posts/PostsRepository.kt
+++ src/main/kotlin/com/l2hyunwoo/kotlinspringbootdemo/domain/posts/PostsRepository.kt
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface PostsRepository : JpaRepository<Posts, Long> {
+    // 조회 기능은 QueryDsl로 개발하는 것이 대세라고 함
     @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
     fun findAllDesc(): List<Posts>
 }
